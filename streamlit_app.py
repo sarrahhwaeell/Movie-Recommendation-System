1debe10b93e34d93a4268252b13d3e7305364b16
Sarah <179869303+sarrahhwaeell@users.noreply.github.com>
2024-12-07 22:28:03 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,29 +1,29 @@
 import streamlit as st
 import pickle
-import pandas as pd
 
-# Load the pre-trained models
-with open('movie_list.pkl', 'rb') as f:
+# Set the paths to the downloaded .pkl files
+movie_list_path = 'path_to_your_local_drive/Movie recommendation system/movie_list.pkl'
+similarity_matrix_path = 'path_to_your_local_drive/Movie recommendation system/similarity_matrix.pkl'
+
+# Load the .pkl files
+with open(movie_list_path, 'rb') as f:
     movie_list = pickle.load(f)
 
-with open('similarity_matrix.pkl', 'rb') as f:
+with open(similarity_matrix_path, 'rb') as f:
     similarity_matrix = pickle.load(f)
 
-# Set the title for your Streamlit app
+# Display Streamlit app content
 st.title('Movie Recommendation System')
 
-# Create an input text box for the user to enter the movie name
 movie_name = st.text_input('Enter a Movie Name')
 
-# Function to get movie recommendations
+# Recommendation logic remains the same
 def recommend(movie_name):
     try:
-        # Find movie index and make recommendations
         movie_idx = movie_list[movie_list['title'] == movie_name].index[0]
         similarity_scores = list(enumerate(similarity_matrix[movie_idx]))
         similarity_scores = sorted(similarity_scores, key=lambda x: x[1], reverse=True)
         
-        # Get top 5 recommended movies
         recommended_movies = []
         for i in range(1, 6):
             movie_idx = similarity_scores[i][0]
@@ -34,10 +34,7 @@ def recommend(movie_name):
     except Exception as e:
         return f"Error: {str(e)}"
 
-# When the user clicks the button to get recommendations
 if movie_name:
     recommendations = recommend(movie_name)
     st.write(f"Recommendations for '{movie_name}':")
     st.write(recommendations)
-
-
