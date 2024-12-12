09b2cb6639ae0ad299f3c232b44f2d50248bad17
Sarah <179869303+sarrahhwaeell@users.noreply.github.com>
2024-12-12 16:02:03 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,13 +15,14 @@ def load_pickle(file_url=None, file_name=None):
     raise ValueError("Both file_url and file_name are missing!")
 
 # Paths and URLs for your .pkl files
-movie_list_path = "movies_list.pkl"  # Local file path
+movie_list_url = "https://github.com/sarrahhwaeell/Movie-Recommendation-System/releases/download/v1.0.0-initial-release/movie_list.pkl"
+movie_list_path = "movies_list.pkl"  # Local filename
 similarity_matrix_url = "https://github.com/sarrahhwaeell/Movie-Recommendation-System/releases/download/v1.0.0-initial-release/similarity_matrix.pkl"
 similarity_matrix_path = "similarity_matrix.pkl"
 
 # Load .pkl files
-movie_list = load_pickle(file_name=movie_list_path)  # Local file for movies_list.pkl
-similarity_matrix = load_pickle(file_url=similarity_matrix_url, file_name=similarity_matrix_path)  # URL for similarity_matrix.pkl
+movie_list = load_pickle(file_url=movie_list_url, file_name=movie_list_path)  # Download and load movies_list.pkl
+similarity_matrix = load_pickle(file_url=similarity_matrix_url, file_name=similarity_matrix_path)  # Download and load similarity_matrix.pkl
 
 # Streamlit app
 st.title('Movie Recommendation System')
